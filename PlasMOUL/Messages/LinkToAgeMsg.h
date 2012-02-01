83cbbd9926fea6461687cdeb7c86b78f93982e89
Adam Johnson <AdamJohnso@gmail.com>
2012-02-01 18:09:34 -0500

    Implement LinkToAgeMsg and friends

---
+++ PlasMOUL/Messages/LinkToAgeMsg.h
@@ -0,0 +1,44 @@
+/******************************************************************************
+ * This file is part of dirtsand.                                             *
+ *                                                                            *
+ * dirtsand is free software: you can redistribute it and/or modify           *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * dirtsand is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with dirtsand.  If not, see <http://www.gnu.org/licenses/>.          *
+ ******************************************************************************/
+
+#ifndef _MOUL_LINKTOAGEMSG_H
+#define _MOUL_LINKTOAGEMSG_H
+
+#include "AgeLinkStruct.h"
+#include "Message.h"
+
+namespace MOUL
+{
+    class LinkToAgeMsg : public Message
+    {
+        FACTORY_CREATABLE(LinkToAgeMsg)
+        
+        // TODO: Find a sane way to implement makeSafeForNet
+        virtual void read(DS::Stream* s);
+        virtual void write(DS::Stream* s);
+        
+    public:
+        AgeLinkStruct* m_ageLink;
+        DS::String m_linkInAnim;
+        
+    protected:
+        LinkToAgeMsg(uint16_t type) : Message(type), m_ageLink(AgeLinkStruct::Create()) { }
+        ~LinkToAgeMsg();
+    };
+};
+
+#endif // _MOUL_LINKTOAGEMSG_H
