114cbfe0222c7bca5d065e1ebd7d67fd16f9f074
Michael Hansen <zrax0111@gmail.com>
2012-03-20 19:24:10 -0700

    Relicense under the AGPLv3+ -- closes #60

--- bin/dsData.sh
+++ bin/dsData.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# *****************************************************************************
+# This file is part of dirtsand.
+#
+# dirtsand is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# dirtsand is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with dirtsand.  If not, see <http://www.gnu.org/licenses/>.
+# *****************************************************************************
 # MOUL DataServer Manifest File line creator
 # By Michael Hansen
 #
